d015f0638460b623072ca568ad683ff8b458565b
banzayche <dharm.serg@gmail.com>
2016-03-11 16:22:53 +0200

    Added .less styles, structure and methods for NotesEditor.

--- notes-app/client/components/App.jsx
+++ notes-app/client/components/App.jsx
@@ -4,11 +4,14 @@ import NoteEditor from './NotesEditor.jsx';
 import NoteGrid from './NotesGrid.jsx';
 
 const App = React.createClass({
+	handleNoteAdd(data) {
+		console.log('hey');
+	},
 	render() {
 		return (
 			<div className="App">
 				<h2 className="App_header"> Notes Note </h2>
-				<NoteEditor />
+				<NoteEditor onNotesAdd={this.handleNoteAdd}/>
 				<NoteGrid />
 			</div>
 		);
--- notes-app/client/components/NotesEditor.jsx
+++ notes-app/client/components/NotesEditor.jsx
@@ -1,8 +1,63 @@
 import React from 'react';
 
+import './NotesEditor.less'
+
 const NotesEditor = React.createClass({
+	getInitialState() {
+		return {
+			title: '',
+			text: '',
+			color: '#ffffff'
+		};
+	},
+
+	handleTextChange(event) {
+		this.setState({ text: event.target.value })
+	},
+
+	handleTitleChange(event) {
+		this.setState({ text: event.target.value })
+	},
+
+	handleAddNotes() {
+		const newNote = {
+			title: this.state.title,
+			text: this.state.text,
+			color: this.state.color
+		};
+
+		this.props.onNotesAdd(newNote);
+		this.setState({ text: '', title: '', color: '#FFFFFF' });
+	},
+
 	render() {
-		return <h1> Notes NotesEditor </h1>;
+		return (
+			<div className="NoteEditor">
+				<input
+					type='text'
+					className='NoteEditor__title'
+					placeholder='Enter title'
+					value={this.state.title}
+					onChange={this.handleTitleChange}
+				/>
+				<textarea
+					placeholder='Enter note text'
+					rows={5}
+					className='NoteEditor__text'
+					value={this.state.text}
+					onChange={this.handleTextChange}
+				/>
+				<div className='NoteEditor__footer'>
+					<button
+						className='NoteEditor__button'
+						disabled={!this.state.text}
+						onClick={this.handleAddNotes}
+					>
+						Add
+					</button>
+				</div>
+			</div>
+		);
 	}
 });
 
